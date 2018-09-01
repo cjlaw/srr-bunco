6bca3c4bee9f92cf330e15fe284651d7de6067fa
Casey Lawrence <casey.j.lawrence@gmail.com>
2018-08-31 21:59:58 -0500

    Remove "in-memory-web-api" dependency & usage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,20 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
-
-import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
-import { InMemoryDataService } from "./services/in-memory-data.service";
-
 import { AppRoutingModule } from "./app-routing.module";
 
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatInputModule, MatTableModule, MatButtonModule } from "@angular/material";
-import {CdkTableModule} from '@angular/cdk/table';
+import {
+  MatInputModule,
+  MatTableModule,
+  MatButtonModule
+} from "@angular/material";
+import { CdkTableModule } from "@angular/cdk/table";
 
 import { AppComponent } from "./app.component";
 import { SignupComponent } from "./components/signup/signup.component";
 import { RsvpTableComponent } from "./components/rsvp-table/rsvp-table.component";
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
 
 @NgModule({
   imports: [
@@ -23,18 +23,11 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
-
-    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
-    // and returns simulated server responses.
-    // Remove it when a real server is ready to receive requests.
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
-      dataEncapsulation: false
-    }),
     BrowserAnimationsModule,
     MatInputModule,
     MatTableModule,
     MatButtonModule,
-    CdkTableModule,
+    CdkTableModule
   ],
   declarations: [
     AppComponent,
