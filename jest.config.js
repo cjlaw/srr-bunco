da1e6a1dd06343c20ed743357ba72be25201adbd
Casey Lawrence <casey.j.lawrence@gmail.com>
2018-09-24 22:38:07 -0500

    Revert "Replace Karma/Jasmine with Jest"
    This reverts commit d3b7d13d11126da0de1fcc568ee7e893b127175d.

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: "jest-preset-angular",
-  roots: ['src'],
-  setupTestFrameworkScriptFile: "<rootDir>/src/setup-jest.ts",
-  moduleNameMapper: {
-    "@lib/(.*)": "<rootDir>/src/lib/$1"
-  }
-}
